f38e2c4be46a58fd69746428eb377f6fe7581ffa
Chukwuma Ezumezu <chukwuma.ezumezu@andela.com>
2016-08-29 19:57:06 -0700

    add inverted-index.js body

---
+++ src/inverted-index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+function Index () {
+  var _ = require('lodash');
+  var fs = require('fs');
+  var path = require('path');
+  var currentFile = '';
+  var currentDataIndex = {};
+  
+  this.wordIndex = {};
+  
+  this.createIndex = function(filePath){
+    
+  };
+  
+  this.getIndex = function(fileName){
+    
+  };
+  
+  this.searchIndex = function(term){
+    
+  };
+
+  
+}
+
+module.exports= Index;
