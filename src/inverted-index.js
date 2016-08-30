0cbda8400496260d1ed5df5482f6041760118cd6
Chukwuma Ezumezu <chukwuma.ezumezu@andela.com>
2016-08-29 19:57:49 -0700

    add createIndex

--- src/inverted-index.js
+++ src/inverted-index.js
@@ -10,17 +10,53 @@ function Index () {
   this.wordIndex = {};
   
   this.createIndex = function(filePath){
-    
+    currentFile = path.resolve(filePath);
+    var that = this;
+    fs.readFile(filePath, function(err, data){
+      if (err) console.log('There was an Error Reading File. View Details Below.\n', err.message);
+      if (data.length > 0){
+        var dataObject = JSON.parse(data.toString());
+        that.isEmpty(dataObject);
+      }else{
+        console.log('Empty file.');
+      }
+    });
   };
   
-  this.getIndex = function(fileName){
-    
+  this.isEmpty = function(data) {
+    var trueOrFalse = true;
+    if(Object.prototype.toString.call(data) === Object.prototype.toString.call([])){
+      for( var i = 0; i < data.length; i++){
+        if(Object.prototype.toString.call(data[i]) === Object.prototype.toString.call({})){
+          if(indexData(data[i], i) && (trueOrFalse !== false)) trueOrFalse = false;
+        }
+      }
+      this.wordIndex[currentFile] = currentDataIndex;
+      cleanUpTemp();
+      return trueOrFalse;
+    }else{
+      return true;
+    }  
+  };
+  var cleanUpTemp = function() {
+    currentDataIndex = {};
+    currentFile = '';
   };
   
-  this.searchIndex = function(term){
-    
+  var indexData = function(data, indexNum) {
+    if (Object.keys(data).length < 1) return false;
+    var bookText = _.values(data).toString();
+    var dataList = _.uniq(_.words(bookText.toLowerCase()));
+    for(var i = 0; i < dataList.length; i++){
+      if(Object.keys(currentDataIndex).includes(dataList[i])){
+        currentDataIndex[dataList[i]].push(indexNum);
+      } else {
+        currentDataIndex[dataList[i]] = [indexNum];
+      }
+    }
+    return true;
   };
-
+  
   
 }
 
